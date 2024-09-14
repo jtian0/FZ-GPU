9ac9be92fd964d1a7f1a39961ab9b4084665aed9
J. Tian <tian.jn09+dev@gmail.com>
2024-09-14 06:18:29 +0000

    240914 tentatively create cmake module

--- cmake/cuda.cmake
+++ cmake/cuda.cmake
@@ -5,9 +5,11 @@ find_package(CUDAToolkit REQUIRED)
 include(GNUInstallDirs)
 include(CTest)
 
-# configure_file(${CMAKE_CURRENT_SOURCE_DIR}/src/fzgpu_version.h.in
-# ${CMAKE_CURRENT_BINARY_DIR}/include/fzgpu_version.h)
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/src/fzgpu_version.h.in
+    ${CMAKE_CURRENT_BINARY_DIR}/include/fzgpu_version.h)
+
 add_library(fzgpu_cu_compile_settings INTERFACE)
+add_library(FZGPU::cu_compile_settings ALIAS fzgpu_cu_compile_settings)
 
 target_compile_definitions(
     fzgpu_cu_compile_settings
@@ -24,8 +26,6 @@ target_include_directories(
     $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include/>
     $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/include/>
     $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
-
-    # $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/cusz>)
 )
 
 # FUNC={core,api}, BACKEND={serial,cuda,...}
@@ -34,28 +34,31 @@ add_library(fzgpu_cu_compat
     src/fz.cu
 )
 target_link_libraries(fzgpu_cu_compat
-    PUBLIC fzgpu_cu_compile_settings CUDA::cudart
+    PUBLIC FZGPU::cu_compile_settings CUDA::cudart
 )
+add_library(FZGPU::cu_compat ALIAS fzgpu_cu_compat)
 
 add_library(fzgpu_cu_module
     src/fz_module.cu
 )
 target_link_libraries(fzgpu_cu_module
-    PUBLIC fzgpu_cu_compile_settings
+    PUBLIC FZGPU::cu_compile_settings
 )
+add_library(FZGPU::cu_module ALIAS fzgpu_cu_module)
 
 add_library(fzgpu_cu_driver
     src/fz_driver.cc
     src/fz_utils.cc
 )
 target_link_libraries(fzgpu_cu_driver
-    PUBLIC fzgpu_cu_compile_settings
+    PUBLIC FZGPU::cu_compile_settings
     CUDA::cudart
 )
+add_library(FZGPU::cu_driver ALIAS fzgpu_cu_driver)
 
 add_executable(fzcli src/fz_cli.cc)
 set_source_files_properties(src/fz_cli.cc PROPERTIES LANGUAGE CUDA)
-target_link_libraries(fzcli PRIVATE fzgpu_cu_driver fzgpu_cu_module)
+target_link_libraries(fzcli PRIVATE FZGPU::cu_driver FZGPU::cu_module)
 set_target_properties(fzcli PROPERTIES OUTPUT_NAME fz)
 
 # enable examples and testing
@@ -68,8 +71,47 @@ if(BUILD_TESTING)
 endif()
 
 # installation
-install(TARGETS fzgpu_cu_compile_settings EXPORT CUSZTargets)
-install(TARGETS fzgpu_cu_compat EXPORT CUSZTargets LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR})
+install(TARGETS
+    fzgpu_cu_compile_settings
+    fzgpu_cu_compat
+    fzgpu_cu_module
+    fzgpu_cu_driver
+    EXPORT FZGPUTargets
+    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+    INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+)
 
-# install(TARGETS cusz EXPORT CUSZTargets LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR})
-# install(TARGETS cusz-bin EXPORT CUSZTargets)
+# install the executable
+install(TARGETS fzcli
+    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+)
+
+# export the targets to a script
+install(EXPORT FZGPUTargets
+    FILE FZGPUTargets.cmake
+    NAMESPACE FZGPU::
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/FZGPU
+)
+
+# generate and install package configuration files
+include(CMakePackageConfigHelpers)
+
+write_basic_package_version_file(
+    "${CMAKE_CURRENT_BINARY_DIR}/FZGPUConfigVersion.cmake"
+    VERSION ${PROJECT_VERSION}
+    COMPATIBILITY AnyNewerVersion
+)
+
+configure_package_config_file(
+    "${CMAKE_CURRENT_SOURCE_DIR}/cmake/FZGPUConfig.cmake.in"
+    "${CMAKE_CURRENT_BINARY_DIR}/FZGPUConfig.cmake"
+    INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/FZGPU
+)
+
+install(FILES
+    "${CMAKE_CURRENT_BINARY_DIR}/FZGPUConfig.cmake"
+    "${CMAKE_CURRENT_BINARY_DIR}/FZGPUConfigVersion.cmake"
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/FZGPU
+)
